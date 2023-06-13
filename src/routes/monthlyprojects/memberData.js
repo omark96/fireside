0041a099adb1f1348aa6068657d22e1a170ff875
omark96 <olle.markstrom96@gmail.com>
2023-06-13 22:53:23 +0200

    rip3

---
+++ src/routes/monthlyprojects/memberData.js
@@ -0,0 +1,12 @@
+export const memberSquares = [
+    {
+        name: "Olle",
+        dir: "olle", 
+        images: [11, 13, 24],
+    },
+    {
+        name: "Anton",
+        dir: "anton",
+        images: [],
+    }
+];
---
+++ src/routes/monthlyprojects/squaresData.js
@@ -0,0 +1,102 @@
+export const bingoSquares = [
+    {
+        id: 0,
+        text: "Bird",
+    },
+    {
+        id: 1,
+        text: "Selfie in the sun",
+    },
+    {
+        id: 2,
+        text: "Green Door",
+    },
+    {
+        id: 3,
+        text: "Something Colorful",
+    },
+    {
+        id: 4,
+        text: "Cliff",
+    },
+    {
+        id: 5,
+        text: "Garbage Can",
+    },
+    {
+        id: 6,
+        text: "Ends in \"k\"",
+    },
+    {
+        id: 7,
+        text: "Tower",
+    },
+    {
+        id: 8,
+        text: "Bridge",
+    },
+    {
+        id: 9,
+        text: "Park Bench",
+    },
+    {
+        id: 10,
+        text: "Brick Wall",
+    },
+    {
+        id: 11,
+        text: "Body of Water",
+    },
+    {
+        id: 12,
+        text: "FREE",
+    },
+    {
+        id: 13,
+        text: "Blue Flower",
+    },
+    {
+        id: 14,
+        text: "Dog",
+    },
+    {
+        id: 15,
+        text: "Insect",
+    },
+    {
+        id: 16,
+        text: "Something Weird",
+    },
+    {
+        id: 17,
+        text: "Big Rock",
+    },
+    {
+        id: 18,
+        text: "Pinecone",
+    },
+    {
+        id: 19,
+        text: "Number 16",
+    },
+    {
+        id: 20,
+        text: "Clouds",
+    },
+    {
+        id: 21,
+        text: "Berries",
+    },
+    {
+        id: 22,
+        text: "Something Old",
+    },
+    {
+        id: 23,
+        text: "Starts with \"p\"",
+    },
+    {
+        id: 24,
+        text: "Boat",
+    },
+]
