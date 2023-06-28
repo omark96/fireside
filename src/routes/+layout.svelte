e7321bba10c23fb5f988665a2b96b69ae449a23f
omark96 <olle.markstrom96@gmail.com>
2023-06-29 00:37:17 +0200

    transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,22 +3,26 @@
 	import '../app.css';
 	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';
+	import PageTransition from './Transition.svelte';
 	import { page } from '$app/stores';
+	export let data;
 </script>
 
 <!-- Header -->
 <Header />
 
 <!-- Black hole for other content -->
-{#if $page.url.pathname === '/'}
-	<main>
-		<slot />
-	</main>
-{:else}
-	<main class="container">
-		<slot />
-	</main>
-{/if}
+<PageTransition url={data.url}>
+	{#if $page.url.pathname === '/'}
+		<main>
+			<slot />
+		</main>
+	{:else}
+		<main class="container">
+			<slot />
+		</main>
+	{/if}
+</PageTransition>
 <!-- Footer -->
 <Footer />
 
