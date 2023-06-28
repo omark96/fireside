985679544643fd6267cf21a40b531593c7ff96d2
omark96 <olle.markstrom96@gmail.com>
2023-06-28 23:55:22 +0200

    may

---
+++ src/posts/monthlyprojects/Youtube.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let link;
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<iframe
+	src="https://www.youtube.com/embed/{link}"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+	allowfullscreen
+/>
+
+<style>
+	iframe {
+		height: 160px;
+		width: 300px;
+	}
+	@media screen and (min-width: 768px) {
+		iframe {
+			height: 360px;
+			width: 640px;
+		}
+	}
+</style>
