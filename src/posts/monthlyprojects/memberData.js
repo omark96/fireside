b046af9486d4a3ad7bd98c7c02134ad663fe6c87
omark96 <olle.markstrom96@gmail.com>
2023-07-02 20:12:57 +0200

    anton

--- src/posts/monthlyprojects/memberData.js
+++ src/posts/monthlyprojects/memberData.js
@@ -7,7 +7,7 @@ export const memberSquares = [
     {
         id: 1,
         name: "anton",
-        images: [18],
+        images: [3, 4, 5, 6, 7, 8, 9, 11, 13, 14, 16, 17, 18, 19, 20, 21, 22],
     },
     {
         id: 2,
